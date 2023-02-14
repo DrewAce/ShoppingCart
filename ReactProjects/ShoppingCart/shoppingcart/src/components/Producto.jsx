0efb4fb79dc9297c07670cbd9e05f8180e3bec08
unknown <juancarmona3090@gmail.com>
2023-02-14 08:58:53 -0500

    Shopping Cart

---
+++ ReactProjects/ShoppingCart/shoppingcart/src/components/Producto.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+import Button from './Button'
+const styles= {
+producto:{
+    border:'solid 1px #eee',
+    boxShadow: '0 5px rgb(0,0,0, 0.1)',
+    width: '30%',
+    padding: '10px 15px',
+    borderRadius: '5px'
+},
+img:{
+    width: '100%',
+}
+}
+
+class Producto extends Component{
+    render(){
+        const {producto, agregarAlCarro}= this.props
+
+        console.log(this.props);
+        return(
+            <div style={styles.producto}>
+             <img style={styles.img}alt={producto.name} src={producto.img }></img>
+             <h3>{producto.name}</h3>
+             <p>{producto.price}</p>
+             <Button onClick={()=> agregarAlCarro(producto)}>
+                Add To the Shopping Cart
+             </Button>
+            </div>
+                
+            
+        )
+    }
+}
+export default Producto
