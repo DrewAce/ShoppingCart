588c7060850f8339c19cb6af99b98408ab6865f0
unknown <juancarmona3090@gmail.com>
2023-03-14 18:17:04 -0500

    Shopping Cart Refactor

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { Component } from 'react'
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
+
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1, img: '/productos/tomate.jpg' },
+      { name: 'Arvejas', price: 2, img: '/productos/arvejas.jpg' },
+      { name: 'Lechuga', price: 0.5, img: '/productos/leshuga.jpg' },
+    ],
+    carro: [],
+    esCarroVisible: false,
+  }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+      return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
+
+  mostrarCarro = () => {
+    if(!this.state.carro.length) {
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible })
+  }
+
+  render() {
+    const { esCarroVisible } = this.state
+    
+    return (
+      <div>
+        <Navbar
+          carro={this.state.carro}
+          esCarroVisible={esCarroVisible}
+          mostrarCarro={this.mostrarCarro}
+        />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={this.agregarAlCarro}
+            productos={this.state.productos}
+          />
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App;
+
---
+++ src/components/BubbleAlert.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+const styles={
+    bubblealert:{
+        backgroundColor: '#E9725A',
+        borderRadius: '15px',
+        color:'#fff',
+        padding:'2px 10px',
+        fontSize: '0.9rem',
+        width: '20px',
+
+    }
+} 
+
+class BubbleAlert extends Component{
+
+    getNumber(n){
+        if(!n){return ''}
+        return n >9 ? '9+' :n
+    }
+
+
+    render(){
+        const {value} =this.props
+    return(
+        <span style={styles.bubblealert}>
+            {this.getNumber(value)}
+        </span>
+    )
+}
+
+
+
+}
+
+export default BubbleAlert
